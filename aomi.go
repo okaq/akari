d8279808216ef27c06aa2316abc3af97dda7b55c
AQ <aq@okaq.com>
2018-08-13 21:13:02 -0400

    Aomi web handler

--- aomi.go
+++ aomi.go
@@ -2,4 +2,25 @@
 // AQ <aq@okaq.com>
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+	INDEX = "aomi.html"
+)
+
+func AomiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w, r, INDEX)
+}
+
+func main() {
+	fmt.Println("start okaq web on localhost:8080")
+	// time
+	http.HandleFunc("/", AomiHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
 
