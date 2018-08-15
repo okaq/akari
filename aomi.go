41b23e16e245c2566691855e067867082bbad049
AQ <aq@okaq.com>
2018-08-14 21:28:19 -0400

    Add time stamp

--- aomi.go
+++ aomi.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 const (
@@ -19,6 +20,7 @@ func AomiHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("start okaq web on localhost:8080")
 	// time
+	fmt.Printf("the current time is: %s\n", time.Now().String())
 	http.HandleFunc("/", AomiHandler)
 	http.ListenAndServe(":8080", nil)
 }
