97ceee6c80cc97184ce4892240759e23d091b6ae
AQ <aq@okaq.com>
2018-09-08 09:15:11 -0400

    New file daiji.html

---
+++ daiji.go
@@ -0,0 +1,35 @@
+// okaq web server
+// "space races"
+// daiji = "important" (jp)
+// AQ <aq@okaq.com>
+// 2018-09-10
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	INDEX = "daiji.html"
+)
+
+func motd() {
+	fmt.Printf("okaq web serve on localhost:8080\n%s\n", time.Now().String())
+}
+
+func DaijiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+}
+
+func main() {
+	motd()
+	http.HandleFunc("/", DaijiHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+// grid generate and api
+// player id and peer net broker
+
+
