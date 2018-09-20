92a2456585e9f073b8ed8fc6e1153759e034d5f8
AQ <aq@okaq.com>
2018-09-20 19:42:16 -0400

    New file eiga.html

---
+++ eiga.go
@@ -0,0 +1,32 @@
+// okaq web server
+// webgl labyrinth
+// eiga = image (jp)
+// AQ <aq@okaq.com>
+// 2018-09-20
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	INDEX = "eiga.html"
+)
+
+func motd() {
+	fmt.Printf("okaq web on localhost:8080\n%s\n", time.Now().String())
+}
+
+func EigaHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
+func main() {
+	motd()
+	http.HandleFunc("/", EigaHandler)
+}
+
+
