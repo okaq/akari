23cfd40ad477b3c6f5acdbd210c219bb97cca8f2
AQ <aq@okaq.com>
2018-11-27 20:54:01 -0500

    New file jiyu.html

---
+++ jiyu.go
@@ -0,0 +1,27 @@
+// okaq akari jiyu
+// mobile web layout spec
+// AQ <aq@okaq.com>
+// 2018-11-28
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+	INDEX = "jiyu.html"
+)
+
+func motd() {
+	fmt.Println("okaq jiyu")
+}
+
+func main() {
+	motd()
+}
+
+// localhost tunnel to public www
+// test on multiple mobile web screens
+
+
