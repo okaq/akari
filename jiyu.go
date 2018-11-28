8e0218eb4cd908d644bfb877a9fbb5cf31b64f91
AQ <aq@okaq.com>
2018-11-28 09:17:22 -0500

    Query and stats notes

--- jiyu.go
+++ jiyu.go
@@ -7,21 +7,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 const (
 	INDEX = "jiyu.html"
 )
 
+func JiyuHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
 func motd() {
-	fmt.Println("okaq jiyu")
+	fmt.Println("okaq jiyu on localhost:8080")
+	fmt.Println(time.Now().String())
 }
 
 func main() {
 	motd()
+	http.HandleFunc("/", JiyuHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 // localhost tunnel to public www
 // test on multiple mobile web screens
+
+// no logging
+// cached live stats served on /s
+
+// triva generator
+// interface for admin (edit,add,delete)
+// upload user generated and curate
+// serve one at a time to client
+// query answer on server
 
 
