4ccf825eb59c8ab4d8837f68ccf247024a8266c0
AQ <aq@okaq.com>
2018-12-31 17:55:49 -0500

    DOM init

--- ofuro.go
+++ ofuro.go
@@ -18,4 +18,11 @@ func OfuroHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w,r,INDEX)
 }
 
+func main() {
+	fmt.Println("okaq web start on localhost:8080")
+	fmt.Println(time.Now().String())
+	http.HandleFunc("/", OfuroHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
 
